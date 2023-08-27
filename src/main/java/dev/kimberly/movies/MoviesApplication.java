633a2eb584cc99801d9f2d9b8168f2812618151e
Kimberly Dijkmans <kim.14420@outlook.com>
2023-08-27 18:13:35 +0100

    remove unused import

--- src/main/java/dev/kimberly/movies/MoviesApplication.java
+++ src/main/java/dev/kimberly/movies/MoviesApplication.java
@@ -2,7 +2,6 @@ package dev.kimberly.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
